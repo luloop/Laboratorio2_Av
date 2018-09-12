404e35a6c92b303b89c3a1195061f99d8cbdc091
Lucila Rizzi <36158159+luloop@users.noreply.github.com>
2018-09-12 12:07:52 -0300

    Add files via upload
    clase 12-09-formularios

---
+++ CLASE-08/Clase07/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase07
+{
+    class Program
+    {
+
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Tempera temp1 = new Tempera(10, ConsoleColor.Red, "LOlo");
+            Tempera temp2= new Tempera(10, ConsoleColor.Red, "LOlo");
+            if (temp1==temp2)
+            {
+                Console.WriteLine("Son iguales");
+            }
+
+        }
+    }
+}
